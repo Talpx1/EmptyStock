740d66a29095661c18cea8c3d352efa354c8d800
Talpx1 <simo3401@gmail.com>
2024-09-24 09:57:01 +0000

    added some bootstrap configuration to AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 final class AppServiceProvider extends ServiceProvider {
     /**
@@ -19,6 +21,15 @@ final class AppServiceProvider extends ServiceProvider {
      * Bootstrap any application services.
      */
     public function boot(): void {
-        Model::shouldBeStrict();
+        Model::shouldBeStrict(! $this->app->isProduction());
+
+        DB::prohibitDestructiveCommands($this->app->isProduction());
+
+        Password::min(8)
+            ->letters()
+            ->mixedCase()
+            ->numbers()
+            ->symbols()
+            ->uncompromised();
     }
 }
